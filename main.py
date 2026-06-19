186076fdfa2af1cd98acf25c70d1c036ac400fa3
Dushyant <dushyant@gmail.com>
2026-06-19 15:14:18 +0530

    removed the extra week2 files

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def home():
-    return { "message":"Hello Welcome!"}
-
-@app.get("/hello/{user}")
-def greeting(user:str):
-   return {"message": f"Hello {user} !"}
