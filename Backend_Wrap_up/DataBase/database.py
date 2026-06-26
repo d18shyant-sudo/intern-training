ab5b6fb05e85b7da52cbb2f14c229c3e03dcfa5b
Dushyant <dushyant@gmail.com>
2026-06-26 15:47:58 +0530

    removing the backend

--- Backend_Wrap_up/DataBase/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker,declarative_base
-from sqlalchemy import create_engine
-import os
-from dotenv import load_dotenv
-Base = declarative_base()
-load_dotenv()
-DATABASE_URL=os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-sessionLocal = sessionmaker(bind=engine)
--- Backend_Wrap_up/DataBase/dependencies.py
+++
@@ -1,7 +0,0 @@
-from database import sessionLocal
-def get_db():
-    try:
-        db = sessionLocal()
-        yield db
-    finally:
-        db.close()
